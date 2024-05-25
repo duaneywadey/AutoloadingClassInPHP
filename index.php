19dea62aa9e322c4596951d8beede1796f31287f
duaneywadey <idrdequito.cvt@eac.edu.ph>
2024-05-25 15:14:15 +0800

    displaying values from the database through print_r()

--- index.php
+++ index.php
@@ -8,14 +8,23 @@ require_once('php/core.php');
 // $showAllQuizzes = $quizObj->showAllQuizzes();
 // print_r($showAllQuizzes); 
 
-// $getQuizByID = $quizObj->getQuizByID(1);
-// print_r($getQuizByID); 
-
 // $showAllQuestionsByQuizID = $questionObj->showAllQuestionsByQuizID(1);
 // print_r($showAllQuestionsByQuizID);
 
-$showAllChoicesByQuestionID = $choiceObj->showAllChoicesByQuestionID(8);
-print_r($showAllChoicesByQuestionID);
+// $showAllChoicesByQuestionID = $choiceObj->showAllChoicesByQuestionID(8);
+// print_r($showAllChoicesByQuestionID);
+
+// Displaying records from the database by ID
+$getQuizByID = $quizObj->getQuizByID(1);
+echo '<pre>';
+print_r($getQuizByID); 
+echo '</pre>';
+
+// Accessing by index number
+echo "<h1>" . $getQuizByID[1] . "</h1>";
+
+// Accessing by column name
+echo "<h1>" . $getQuizByID['date_added'] . "</h1>";
 
 
 ?>
